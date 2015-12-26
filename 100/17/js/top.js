4932646fe4f0757e82f46c0dbc5a13add7245d1d
Heureuxly <1042345030@qq.com>
2015-12-27 03:51:38 +0800

    Revert "Revert "项目 Yamaha""
    This reverts commit 1305154ef128849d45af5a2154a5951e261aa79a.

---
+++ 100/17/js/top.js
@@ -0,0 +1,8 @@
+$(function(){
+	$(".icon-list-ul").click(function(){
+		$(".mobilediv").toggleClass("hide");
+		$("#wrapper").toggleClass("mobilewrapper");
+		$(".header-show").toggleClass("header-showReuturn")
+		})
+
+	})
