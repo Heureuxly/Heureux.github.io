58cf2c4d756565f9f63a87b7e7e00857246ac209
Heureuxly <1042345030@qq.com>
2016-01-04 13:57:18 +0800

    add

---
+++ 100/yamaha/js/top.js
@@ -0,0 +1,12 @@
+$(function(){
+	$(".m1").click(function(){
+		$(".mobilediv").toggleClass("hide");
+		$("#wrapper").toggleClass("mobilewrapper");
+		$(this).toggleClass("m1Reuturn")
+	})
+	$(".m2").click(function(){
+		$(".mobilediv").toggleClass("hide");
+		$("#wrapper").toggleClass("mobilewrapper");
+	})
+
+	})
