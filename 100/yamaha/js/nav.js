2a479f93353d472720c379ab0d0ee52e1868e5e6
Heureuxly <1042345030@qq.com>
2016-03-08 18:40:36 +0800

    change

--- 100/yamaha/js/nav.js
+++ 100/yamaha/js/nav.js
@@ -8,3 +8,33 @@ $(function(){
 	$(".dv").toggleClass("dvchange");
 })
 })
+
+
+
+
+
+var curIndex=0;
+//时间间隔 单位毫秒
+var timeInterval=2000;
+var arr=new Array();
+arr[0]="images/small/wrapper2.jpg";
+arr[1]="images/small/wrapper1.jpg";
+/*arr[2]="3.jpg";
+arr[3]="4.jpg";
+arr[4]="5.jpg";
+arr[5]="6.jpg";
+arr[6]="7.jpg";*/
+setInterval(changeImg,timeInterval);
+function changeImg()
+{
+    var wrapper=document.getElementById("wrapper");
+    if (curIndex==arr.length-1) 
+    {
+        curIndex=0;
+    }
+    else
+    {
+        curIndex+=1;
+    }
+    wrapper.src=arr[curIndex];
+}
