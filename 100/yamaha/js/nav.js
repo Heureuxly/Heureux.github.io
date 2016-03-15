ac3a56414f80f6f70d2642f91d1cd5148d034753
Heureuxly <1042345030@qq.com>
2016-03-16 00:40:45 +0800

    change

--- 100/yamaha/js/nav.js
+++ 100/yamaha/js/nav.js
@@ -13,28 +13,60 @@ $(function(){
 
 
 
-var curIndex=0;
-//时间间隔 单位毫秒
-var timeInterval=2000;
-var arr=new Array();
-arr[0]="images/small/wrapper2.jpg";
-arr[1]="images/small/wrapper1.jpg";
-/*arr[2]="3.jpg";
-arr[3]="4.jpg";
-arr[4]="5.jpg";
-arr[5]="6.jpg";
-arr[6]="7.jpg";*/
-setInterval(changeImg,timeInterval);
-function changeImg()
-{
-    var wrapper=document.getElementById("wrapper");
-    if (curIndex==arr.length-1) 
-    {
-        curIndex=0;
-    }
-    else
-    {
-        curIndex+=1;
-    }
-    wrapper.src=arr[curIndex];
-}
+
+$(document).ready(function(){
+
+    $(".main_visual").hover(function(){
+        $("#btn_prev,#btn_next").fadeIn()
+    },function(){
+        $("#btn_prev,#btn_next").fadeOut()
+    });
+    
+    $dragBln = false;
+    
+    $(".main_image").touchSlider({
+        flexible : true,
+        speed : 200,
+        btn_prev : $("#btn_prev"),
+        btn_next : $("#btn_next"),
+        paging : $(".flicking_con a"),
+        counter : function (e){
+            $(".flicking_con a").removeClass("on").eq(e.current-1).addClass("on");
+        }
+    });
+    
+    $(".main_image").bind("mousedown", function() {
+        $dragBln = false;
+    });
+    
+    $(".main_image").bind("dragstart", function() {
+        $dragBln = true;
+    });
+    
+    $(".main_image a").click(function(){
+        if($dragBln) {
+            return false;
+        }
+    });
+    
+    timer = setInterval(function(){
+        $("#btn_next").click();
+    }, 5000);
+    
+    $(".main_visual").hover(function(){
+        clearInterval(timer);
+    },function(){
+        timer = setInterval(function(){
+            $("#btn_next").click();
+        },5000);
+    });
+    
+    $(".main_image").bind("touchstart",function(){
+        clearInterval(timer);
+    }).bind("touchend", function(){
+        timer = setInterval(function(){
+            $("#btn_next").click();
+        }, 5000);
+    });
+    
+});
